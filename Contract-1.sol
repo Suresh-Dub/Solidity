a04a782232da02000163740e05889d48c07924a9
Suresh-Dub <100891096+Suresh-Dub@users.noreply.github.com>
2023-07-03 21:03:11 +0530

    Create Contract-1.sol

---
+++ Contract-1.sol
@@ -0,0 +1,6 @@
+//Solidity's code is encapsulated in contracts. A contract is the fundamental building block of Ethereum applications — all variables and functions belong to a contract, and this will be the starting point of all your projects.
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract helloworld {
+}
